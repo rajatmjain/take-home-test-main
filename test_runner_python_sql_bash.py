9c930b01cbebc13fd626c5fa28bc49b0b5543eae
Rajat Jain <rajat.apple007@gmail.com>
2023-10-12 17:55:30 -0700

    Final

--- test_runner_python_sql_bash.py
+++ test_runner_python_sql_bash.py
@@ -19,32 +19,32 @@ def test_runner_python_sql_bash():
     runner = CodeRunner(code_text)
     runner.run()
 
-@pytest.mark.python
-def test_runner_python():
-    code = """
-    python```
-    print("Hello, Python!")
-    ```
-    """
-    runner = CodeRunner(code)
-    runner.run()
+# @pytest.mark.python
+# def test_runner_python():
+#     code = """
+#     python```
+#     print("Hello, Python!")
+#     ```
+#     """
+#     runner = CodeRunner(code)
+#     runner.run()
 
-@pytest.mark.sql
-def test_runner_sql():
-    code = """
-    sql```
-    SELECT * FROM my_table;
-    ```
-    """
-    runner = CodeRunner(code)
-    runner.run()
+# @pytest.mark.sql
+# def test_runner_sql():
+#     code = """
+#     sql```
+#     SELECT * FROM my_table;
+#     ```
+#     """
+#     runner = CodeRunner(code)
+#     runner.run()
 
-@pytest.mark.bash
-def test_runner_bash():
-    code = """
-    bash```
-    echo "Hello, Bash!"
-    ```
-    """
-    runner = CodeRunner(code)
-    runner.run()
+# @pytest.mark.bash
+# def test_runner_bash():
+#     code = """
+#     bash```
+#     echo "Hello, Bash!"
+#     ```
+#     """
+#     runner = CodeRunner(code)
+#     runner.run()
